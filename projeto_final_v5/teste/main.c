8ad80ad2041ad6da0089d9ebb5fad0f2bb306b84
joaoferreira2000 <105673750+joaoferreira2000@users.noreply.github.com>
2022-05-30 18:50:19 +0100

    v6

--- projeto_final_v5/teste/main.c
+++ projeto_final_v5/teste/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
@@ -26,57 +27,80 @@ int main()
     }
         printf("%d\n",f_ano);
         printf("%d,numero=i\n",i);
-z=i-1;
+z=i;
         printf("%d,numero=z\n",z);
-                          for (x=i;x>=0;x--)
+
+
+                        for (x=i;x>=0;x--)
+                        {
+                            if (z=3)
                             {
+                                y=1000;
+                            }
+                            else if (z=2)
+                            {
+                                y=100;
+                            }
+                            else if (z=1)
+                            {
+                                y=10;
+                            }
+
+                            else if (z=0)
+                            {
+                                y=0;
+                            }
+
+                            printf("Y=========%d\n",y);
                                 switch (f_cano[x])
                                 {
+
+
                                     case '1' :
-                                    f_ano=f_ano+1*(10*z);
+                                    f_ano=(f_ano+1*y);
                                     printf("1\n");
                                     break;
                                     case '2' :
-                                    f_ano=f_ano+2*(10*z);
+                                    f_ano=f_ano+2*y;
                                     printf("2\n");
                                     break;
                                     case '3' :
-                                    f_ano=f_ano+3*(10*z);
+                                    f_ano=f_ano+3*y;
                                     printf("3\n");
                                     break;
                                     case '4' :
-                                    f_ano=f_ano+4*(10*z);
+                                    f_ano=f_ano+4*y;
                                     printf("4\n");
                                     break;
                                     case '5' :
-                                    f_ano=f_ano+5*(10*z);
+                                    f_ano=f_ano+5*y;
                                     printf("5\n");
                                     break;
                                     case '6' :
-                                    f_ano=f_ano+6*(10*z);
+                                    f_ano=f_ano+6*y;
                                     printf("6\n");
                                     break;
                                     case '7' :
-                                    f_ano=f_ano+7*(10*z);
+                                    f_ano=f_ano+7*y;
                                     printf("7\n");
                                     break;
                                     case '8' :
-                                    f_ano=f_ano+8*(10*z);
+                                    f_ano=f_ano+8*y;
                                     printf("8\n");
                                     break;
                                     case '9' :
-                                    f_ano=f_ano+9*(10*z);
+                                    f_ano=f_ano+9*y;
                                     printf("9\n");
                                     break;
                                     case '0' :
-                                    f_ano=f_ano+0*(10*z);
+                                    f_ano=f_ano+0*y;
                                     printf("0\n");
                                     break;
                                     return 0;
                                     }
-                             z--;
+                            z--;
 
-                            };
+                        };
 
         printf("\n%d\n",f_ano);
 }
